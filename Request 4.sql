0195091dff07646fcf008ad637f011f1f22659f4
Nahuel Rodriguez <nahuel.rodriguez@teaminternational.com>
2023-05-27 13:51:10 -0300

    updated request 4. is not totally fixed

--- Request 4.sql
+++ Request 4.sql
@@ -1,13 +1,13 @@
 SELECT 
     CONCAT(employee.first_name, ' ', employee.last_name) AS 'Employee name',
     project.name AS 'Project name',
-    project.max_sum_rate - position.rate - CAST(equipment.price AS FLOAT) / 12 AS Difference
+    project.max_sum_rate - position.rate - COALESCE(CAST(SUM(equipment.price) AS FLOAT), 0) / 12 AS Difference
 FROM employee
 INNER JOIN project
     ON employee.project_id = project.id
 INNER JOIN position
     ON employee.position_id = position.id
-INNER JOIN equipment
+LEFT JOIN equipment
     ON employee.id = equipment.user_id
-WHERE position.rate + (CAST(equipment.price AS FLOAT) / 12) > project.max_sum_rate
-GROUP BY employee.id, employee.first_name, employee.last_name, project.name, project.max_sum_rate, position.rate, equipment.price;
+WHERE position.rate + (COALESCE(CAST((SELECT SUM(price) FROM equipment WHERE user_id = employee.id) AS FLOAT), 0) / 12) > project.max_sum_rate
+GROUP BY employee.id, employee.first_name, employee.last_name, project.name, project.max_sum_rate, position.rate;
