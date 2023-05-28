249abbacf0f4f1522f5cd20f656bd90eaaf3b3c5
Nahuel Rodriguez <nahuelrmaury@gmail.com>
2023-05-28 13:01:39 -0300

    request 4 fixed

--- Request 4.sql
+++ Request 4.sql
@@ -1,13 +1,18 @@
 SELECT 
     CONCAT(employee.first_name, ' ', employee.last_name) AS 'Employee name',
     project.name AS 'Project name',
-    project.max_sum_rate - emp_position.rate - COALESCE(CAST(emp_equipment.total_price AS FLOAT), 0) / 12 AS Difference,
-    project.max_sum_rate AS MaxSumRate,
-    emp_position.rate AS PositionRate,
-    COALESCE(CAST(emp_equipment.total_price AS FLOAT), 0) / 12 AS EquipmentPrice
+    project.max_sum_rate - emp_position.rate - COALESCE(CAST(emp_equipment.total_price AS FLOAT), 0) / 12 AS Difference
 FROM employee
 INNER JOIN project
     ON employee.project_id = project.id
+LEFT JOIN (
+    SELECT employee.project_id, SUM(equipment.price) AS total_price
+    FROM employee
+    LEFT JOIN equipment
+        ON employee.id = equipment.user_id
+    GROUP BY employee.project_id
+) AS emp_equipment
+    ON employee.project_id = emp_equipment.project_id
 LEFT JOIN (
     SELECT project_id, SUM(position.rate) AS rate
     FROM employee
@@ -16,13 +21,5 @@ LEFT JOIN (
     GROUP BY project_id
 ) AS emp_position
     ON employee.project_id = emp_position.project_id
-LEFT JOIN position
-    ON employee.position_id = position.id
-LEFT JOIN (
-    SELECT user_id, SUM(price) AS total_price
-    FROM equipment
-    GROUP BY user_id
-) AS emp_equipment
-    ON employee.id = emp_equipment.user_id
 GROUP BY employee.id, employee.first_name, employee.last_name, project.name, project.max_sum_rate, emp_position.rate, emp_equipment.total_price
 HAVING emp_position.rate + COALESCE(CAST(emp_equipment.total_price AS FLOAT), 0) / 12 > project.max_sum_rate;
